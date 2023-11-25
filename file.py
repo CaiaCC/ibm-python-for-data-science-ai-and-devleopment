2024a522887c881b0bcbf793ce2fb2e60b46556e
Caia Chuang <caiachuang@gmail.com>
2023-11-24 19:17:49 -0800

    practice writing file

--- file.py
+++ file.py
@@ -21,8 +21,29 @@ with open('text.txt', 'r') as file:
 '''
 NOTE: read lines, can be called multiple times to read subsequent lines
 
-
-'''
 with open('text.txt', 'r') as file:
     for line in file.readlines():
         print(line)
+'''
+
+
+'''
+NOTE: write file with open()
+
+lines = ['this is line #1\n', 'this is line #2\n', 'this is line #3\n']
+
+with open('text-for-writing.txt', 'w') as file:
+    for line in lines:
+        file.write(line)
+'''
+
+'''
+NOTE: appending data to an existing file
+
+
+'''
+
+new_data = 'this is the new line'
+
+with open('text-for-writing.txt', 'a') as file2:
+    file2.write(new_data +)
