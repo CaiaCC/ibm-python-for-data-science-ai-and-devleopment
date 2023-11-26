c26a55fb69856bf490e4aab69f2b9752e995070c
Caia Chuang <caiachuang@gmail.com>
2023-11-26 09:25:38 -0800

    practice pandas loading data

--- practice_pandas.py
+++ practice_pandas.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import piplite
+
 
 '''
 # Read the CSV file into a DataFrame
@@ -15,6 +17,7 @@ print(s[1:4])
 
 '''
 
+'''
 # Creating a DataFrame from a dictionary
 data = {'Name': ['Alice', 'Bob', 'Charlie', 'David'],
         'Age': [25, 30, 35, 28],
@@ -22,6 +25,30 @@ data = {'Name': ['Alice', 'Bob', 'Charlie', 'David'],
 df = pd.DataFrame(data)
 print(df)
 print(df['Name'])  # Access the 'Name' column
-'''
 df.to_csv('test_data.csv', index=True)
 '''
+
+# Read dat from CSV file
+from pyodide.http import pyfetch
+import pandas as pd
+
+filename = "https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-PY0101EN-SkillsNetwork/labs/Module%204/data/TopSellingAlbums.csv"
+
+async def download(url, filename):
+    response = await pyfetch(url)
+    if response.status == 200:
+        with open(filename, "wb") as f:
+            f.write(await response.bytes())
+
+
+await download(filename, "TopSellingAlbums.csv")
+df = pd.read_csv("TopSellingAlbums.csv")
+
+
+# Read data from Excel File and print the first five rows
+
+xlsx_path = 'https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-PY0101EN-SkillsNetwork/jupyterlite/files/Module%205/data/TopSellingAlbums.xlsx'
+
+await download(xlsx_path, "TopSellingAlbums.xlsx")
+df = pd.read_excel("TopSellingAlbums.xlsx")
+df.head()
