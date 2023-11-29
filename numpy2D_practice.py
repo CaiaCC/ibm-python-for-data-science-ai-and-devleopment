2e54228fc0ee8c61875f4b827bc5a056b866c101
Caia Chuang <caiachuang@gmail.com>
2023-11-29 15:53:47 -0800

    practice numpy 2D array

---
+++ numpy2D_practice.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+a = [
+        [11, 12, 13],
+        [21, 22, 23],
+        [31, 32, 33]
+    ]
+A = np.array(a)
+'''
+print(A.ndim)
+print(A.shape)
+print(A.size)
+'''
+
+# transposed matrix
+B = A.T
+print(B)
